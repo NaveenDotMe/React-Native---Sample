9c0fda8fb8b0d8bada120951e77b736a13fb2227
Brock-99 <elmatadordelmar99@gmail.com>
2022-09-11 07:07:27 +0530

    first Commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text,Button } from 'react-native'
+import Home from "./screens/Home";
+
+
+function App() {
+  return (
+    < View >
+      <Home />
+    </View >
+  );
+}
+
+export default App;
